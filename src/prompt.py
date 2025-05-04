7f0f47b2296adbf57bedb373f36f2792a70cabb6
Rishav Raj Bhagat <rishavrajbhagat123@gmail.com>
2025-05-05 01:09:21 +0530

    added prompt

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+prompt_template="""
+You are a medical expert. Answer the question based on the context provided below. If the answer is not in the context, say "I don't know".
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else
+Helpful answer:
+"""
