18bf45028e6706689595e480dce450c62b5d175b
Rishav Raj Bhagat <rishavrajbhagat123@gmail.com>
2025-05-04 18:57:02 +0530

    setup.py file code added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Medical Chatbot Project',
+    version='0.0.0',
+    author='Rishav Raj Bhagat',
+    author_email='rishavrajbhagat123@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
